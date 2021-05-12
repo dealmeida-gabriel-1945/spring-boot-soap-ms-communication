557a517ff08cdf1b8b5048c5ba77e751f363b319
dealmeida-gabriel-1945 <dealmeida.gabriel.d@gmail.com>
2021-05-12 09:49:42 -0300

    Created the car MS

---
+++ spring-boot-soap-ms-car/src/main/java/com/gabrielalmeida/springbootsoapmscar/SpringBootSoapMsCarApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielalmeida.springbootsoapmscar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSoapMsCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSoapMsCarApplication.class, args);
+	}
+
+}
