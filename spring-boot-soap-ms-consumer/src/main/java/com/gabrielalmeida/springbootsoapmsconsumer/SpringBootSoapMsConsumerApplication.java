2af19ee4fb7c285e4842f2bddda97df42ae6ae25
dealmeida-gabriel-1945 <dealmeida.gabriel.d@gmail.com>
2021-05-12 10:42:07 -0300

    Created the consumer MS

---
+++ spring-boot-soap-ms-consumer/src/main/java/com/gabrielalmeida/springbootsoapmsconsumer/SpringBootSoapMsConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielalmeida.springbootsoapmsconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSoapMsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSoapMsConsumerApplication.class, args);
+	}
+
+}
---
+++ spring-boot-soap-ms-consumer/src/test/java/com/gabrielalmeida/springbootsoapmsconsumer/SpringBootSoapMsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielalmeida.springbootsoapmsconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSoapMsConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
