67180d813ef44e491a43ceccdc2c15306b4946a3
dealmeida-gabriel-1945 <dealmeida.gabriel.d@gmail.com>
2021-05-12 08:44:02 -0300

    first commit

---
+++ spring-boot-soap-ms-person/src/main/java/com/gabrielalmeida/springbootsoapmsperson/SpringBootSoapMsPersonApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielalmeida.springbootsoapmsperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSoapMsPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSoapMsPersonApplication.class, args);
+	}
+
+}
---
+++ spring-boot-soap-ms-person/src/test/java/com/gabrielalmeida/springbootsoapmsperson/SpringBootSoapMsPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielalmeida.springbootsoapmsperson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSoapMsPersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
